2b3b39cb41630bdae9ea452fe645a93efdb48ad2
AndrewGluss <bangluss@gmail.com>
2024-10-31 17:09:59 +0300

    add some endpoints for creatures and explorers

--- src/web/explorer.py
+++ src/web/explorer.py
@@ -1,7 +1,34 @@
 from fastapi import APIRouter
+from ..model.explorer import Explorer
+import src.fake.explorer as service
+"""
+В книге указано import ..fake.explorer as service
+Но PyCharm ругался поэтому в Settings сделал src как Mark as Sourse
+"""
 
 router = APIRouter(prefix="/explorer")
 
 @router.get("/")
-def top():
-    return "top explorer endpoint"
+def get_all() -> list[Explorer]:
+    return service.get_all()
+
+@router.get("/{name}")
+def get_one(name) -> Explorer | None:
+    return service.get_one(name)
+
+# методы пока на заглушках
+@router.post("/")
+def create(explorer: Explorer) -> Explorer:
+    return service.create(explorer)
+
+@router.patch("/")
+def modify(explorer: Explorer) -> Explorer:
+    return service.modify(explorer)
+
+@router.put("/")
+def replace(explorer: Explorer) -> Explorer:
+    return service.replace(explorer)
+
+@router.delete("/{name}")
+def delete(name: str):
+    return service.delete(name)
